ce5c6d370d7f9a212357b98679a7af48db077e4e
Joey Wan <joey.wan@ryerson.ca>
2023-01-06 17:18:17 -0500

    made buttons lighter on click

--- dist/dom.js
+++ dist/dom.js
@@ -12,6 +12,79 @@ skills.style.borderTop = 'solid 100px #212529';
 var skills = document.getElementById('extracurriculars');
 skills.style.borderTop = 'solid 100px #DC3545';
 
+//Javascript to make my buttons turn lighter after being clicked 
+var button = document.getElementById('but').addEventListener('click', buttonClick);
+
+function buttonClick(){
+    document.getElementById('but').style.backgroundColor = '#FF7276';
+
+}
+
+var button1 = document.getElementById('but1').addEventListener('click', buttonClick1);
+
+function buttonClick1(){
+    document.getElementById('but1').style.backgroundColor = '#FF7276';
+
+}
+
+var button2 = document.getElementById('but2').addEventListener('click', buttonClick2);
+
+function buttonClick2(){
+    document.getElementById('but2').style.backgroundColor = '#FF7276';
+
+}
+
+var button3 = document.getElementById('but3').addEventListener('click', buttonClick3);
+
+function buttonClick3(){
+    document.getElementById('but3').style.backgroundColor = '#FF7276';
+
+}
+
+var button4 = document.getElementById('but4').addEventListener('click', buttonClick4);
+
+function buttonClick4(){
+    document.getElementById('but4').style.backgroundColor = '#FF7276';
+
+}
+
+var button5 = document.getElementById('but5').addEventListener('click', buttonClick5);
+
+function buttonClick5(){
+    document.getElementById('but5').style.backgroundColor = '#FF7276';
+
+}
+
+var button6 = document.getElementById('but6').addEventListener('click', buttonClick6);
+
+function buttonClick6(){
+    document.getElementById('but6').style.backgroundColor = '#FF7276';
+
+}
+
+var button7 = document.getElementById('but7').addEventListener('click', buttonClick7);
+
+function buttonClick7(){
+    document.getElementById('but7').style.backgroundColor = '#FF7276';
+
+}
+
+var button8 = document.getElementById('but8').addEventListener('click', buttonClick8);
+
+function buttonClick8(){
+    document.getElementById('but8').style.backgroundColor = '#FF7276';
+
+}
+
+var button9 = document.getElementById('but9').addEventListener('click', buttonClick9);
+
+function buttonClick9(){
+    document.getElementById('but9').style.backgroundColor = '#FF7276';
+
+}
+
+
+
 
 
 
