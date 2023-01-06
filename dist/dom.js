c7704a1b306760bf8d70eac4d16ac1c72defa4cd
Joey Wan <joey.wan@ryerson.ca>
2023-01-06 09:57:11 -0500

    minor changes with js and grammar

---
+++ dist/dom.js
@@ -0,0 +1,25 @@
+//Border at top so navbar doesn't skip the section title
+
+var skills = document.getElementById('about');
+skills.style.borderTop = 'solid 100px #DC3545';
+
+var skills = document.getElementById('skills');
+skills.style.borderTop = 'solid 100px #212529';
+
+var skills = document.getElementById('work');
+skills.style.borderTop = 'solid 100px #212529';
+
+var skills = document.getElementById('extracurriculars');
+skills.style.borderTop = 'solid 100px #DC3545';
+
+
+
+
+
+
+
+
+
+
+
+
